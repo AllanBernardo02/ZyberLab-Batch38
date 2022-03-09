c48d6fdcbf66b9aecd37b82ef00618dd4d00e045
Allan02 <85282679+AllanBernardo02@users.noreply.github.com>
2022-03-09 10:22:52 +0800

    UI TODO APP

---
+++ Frontend/src/pages/batch38.vue
@@ -0,0 +1,74 @@
+<template>
+
+    <q-toolbar class="bg-purple text-white">
+      <q-btn flat round dense icon="assignment_ind" />
+      <q-toolbar-title>
+        Batch 38 TODO App
+      </q-toolbar-title>
+      <q-btn flat round dense icon="apps" class="q-mr-xs" />
+      <q-btn flat round dense icon="more_vert" />
+    </q-toolbar>
+
+    <div class="row  q-gutter-sm  q-pa-sm">
+        <q-input class="col" filled v-model="text" label="What needs to be done" />
+        <q-btn icon="add" label="add"></q-btn>
+   </div>
+        <ul>
+            <li>bernardoss</li>
+            <li>bernardos</li>
+            <li>bernardo</li>
+            <li>bernardo</li>
+            <li>bernardo</li>
+        </ul>
+    
+    
+   
+    <!-- <div class="todos">
+        <q-toolbar class="bg-purple text-white">
+      <q-btn flat round dense icon="assignment_ind" />
+      <q-toolbar-title>
+        Batch 38 TODO App
+      </q-toolbar-title>
+      <q-btn flat round dense icon="apps" class="q-mr-xs" />
+      <q-btn flat round dense icon="more_vert" />
+    </q-toolbar>
+        <input @keyup.enter="add" type="text" v-model="title" placeholder="What you want todo">
+        <button @click="add">Add to list</button>
+
+        <ul>
+            <li style="list-style-type: none" v-for="todo in todos" :key="todo"><q-btn style="width: 50%; margin: auto"><input type="checkbox"><label for="">{{todo}}</label></q-btn></li>
+            <li v-for="todo in todos">{{todo}}</li>
+        </ul>
+
+    </div> -->
+
+ 
+
+
+
+
+</template>
+
+<!-- <script>
+    export default {
+        name: "todo",
+        props: {
+            msg: String
+        },
+        data(){
+            return {
+                title: '',
+                todos:[
+                    // 'Go to Shop'
+                ]
+            }
+        },
+        methods:{
+            add(){
+                this.todos.push(this.title);
+                this.title = "";
+            }
+        }
+    };
+    
+</script> -->
