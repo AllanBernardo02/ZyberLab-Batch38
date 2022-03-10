66a33b1f0d42d8b5f530867e86f43916a698a856
Allan02 <85282679+AllanBernardo02@users.noreply.github.com>
2022-03-10 11:38:53 +0800

    Checkbox, Boot file, 3rd Party Components

--- Frontend/src/pages/batch38.vue
+++ Frontend/src/pages/batch38.vue
@@ -18,14 +18,17 @@
    </div>
     <q-list v-for="todo in todos" :key="todo.id" class="q-ma-sm" bordered separator>
         <!-- first list -->
-        <q-item clickable v-ripple>
+        <q-item clickable v-ripple @mouseover="selected = todo.id" @mouseleave="selected = null">
             <q-item-section avatar>
-                <q-icon name="signal_wifi_off" />
+                <q-checkbox v-model="todo.completed" />
             </q-item-section>
-            <q-item-section>{{todo.title}}</q-item-section> <!-- todo.desc called data binding-->
-            <q-item-section side>Side</q-item-section>
+            <q-item-section :class="{ 'text-strike text-grey':  todo.completed}">{{todo.title}}</q-item-section> <!-- todo.desc called data binding-->
+            <q-item-section side>
+                <q-btn v-show="selected === todo.id" round icon="delete"/>
+            </q-item-section>
         </q-item>
     </q-list>
+    <pie-chart :data="[['Active', countActive], ['Completed', countCompleted]]"></pie-chart>
     
    
     <!-- <div class="todos">
@@ -57,10 +60,14 @@
     <!-- composition API -->
 <script setup>
 
-import { reactive, ref, onMounted } from 'vue'
-import axios from 'axios'
+import { reactive, ref, onMounted, computed, getCurrentInstance } from 'vue' //getCurrentInstance use if u get axios globally
+// import axios from 'axios' // Option 1 for using axios: To get data into server
 
 import greet from 'components/greet.vue'
+
+const app = getCurrentInstance()
+
+const { $axios } =app.appContext.config.globalProperties // Option 2 for using axios globally:
 
 //Using Reactive
 const state = reactive({
@@ -75,6 +82,11 @@ const state = reactive({
     // ]
     })
 
+const countCompleted = computed(() => todos.value.filter(t => t.completed).length)// To compute all todos.value if completed.. sample value is 6
+const countActive = computed(() => todos.value.length - countCompleted.value) // To compute all todos.value in array then minus in the completed.value
+
+const selected = ref(null) // for hover delete icon
+
 //Using ref
 const todos = ref([
             {
@@ -86,7 +98,7 @@ const todos = ref([
 
 // onMounted is to initialize the data from axios and Axios used to get the into server
 onMounted(async () => {
-    const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    const { data } = await /*axios or*/$axios.get("https://jsonplaceholder.typicode.com/todos"); //if we use global axios by default use $axios, else axios
     todos.value = data;
 })
 
